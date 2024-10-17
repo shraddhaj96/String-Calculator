1ff867e44635c04347ceeb603797c3c560a29e7c
shraddha jorwekar <jorvekar.shraddha@gmail.com>
2024-10-17 19:24:58 +0530

    stringcalc function call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'string-calculator';
+  ngOnInit(): void {
+    this.stringCalc();
+  }
+  stringCalc(){
+       console.log("function call")
+  }
 }
